a8d2ab3bb593c14530cde9dce558315daa283341
spaulmark <fdoou1@gmail.com>
2020-04-14 17:14:56 -0400

    Updates

---
+++ precache-manifest.d0bd254f6deb169fa167c8864753f893.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c5b3ae24ad353434636",
+    "url": "/project-lsg/static/js/runtime~main.32522c35.js"
+  },
+  {
+    "revision": "e7f89110fdf38ffa8d06",
+    "url": "/project-lsg/static/js/main.170c5fd2.chunk.js"
+  },
+  {
+    "revision": "3ccb6e06f1d39f4a2986",
+    "url": "/project-lsg/static/js/2.3974beac.chunk.js"
+  },
+  {
+    "revision": "e7f89110fdf38ffa8d06",
+    "url": "/project-lsg/static/css/main.ad40f0b4.chunk.css"
+  },
+  {
+    "revision": "2932c02d5fb745be6ac74b4236e31de3",
+    "url": "/project-lsg/index.html"
+  }
+];
