51dc5ee0a989c807bd02b8260ba91834564f400c
Spencer <fdoou1@gmail.com>
2020-06-12 20:40:08 -0400

    Updates

---
+++ precache-manifest.90a5de7463f6c2a939abdb47b0ada7ef.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b28959b0f18049c9010ed00692edf9d",
+    "url": "/project-lsg/index.html"
+  },
+  {
+    "revision": "67f8817ffe7545233aa4",
+    "url": "/project-lsg/static/css/main.e17d5081.chunk.css"
+  },
+  {
+    "revision": "5e15b204cff3f0bf969b",
+    "url": "/project-lsg/static/js/2.9baa55ca.chunk.js"
+  },
+  {
+    "revision": "8f515e83b74865f719c11b4db402613b",
+    "url": "/project-lsg/static/js/2.9baa55ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f8817ffe7545233aa4",
+    "url": "/project-lsg/static/js/main.5d7bc412.chunk.js"
+  },
+  {
+    "revision": "4f7dd62a379c65452836",
+    "url": "/project-lsg/static/js/runtime-main.8aaf33b9.js"
+  },
+  {
+    "revision": "9b5ea610960472fe50b121abaa91abef",
+    "url": "/project-lsg/static/media/Ancient Oven.9b5ea610.png"
+  },
+  {
+    "revision": "419ba63829b8aaeb4e02118375b8379c",
+    "url": "/project-lsg/static/media/Chio.419ba638.png"
+  },
+  {
+    "revision": "2c4852e730426e16b9608cbd3aee7490",
+    "url": "/project-lsg/static/media/Epona.2c4852e7.png"
+  },
+  {
+    "revision": "52325a3731a52040819485331081efd0",
+    "url": "/project-lsg/static/media/Hunnie.52325a37.png"
+  },
+  {
+    "revision": "9bee880bbe61777086afe70c53b17a5c",
+    "url": "/project-lsg/static/media/Karson.9bee880b.png"
+  },
+  {
+    "revision": "8c4ea5176c450e759682b71fc744ce40",
+    "url": "/project-lsg/static/media/Kass.8c4ea517.png"
+  },
+  {
+    "revision": "81cb17e71a5f7474029b817fcf845ffd",
+    "url": "/project-lsg/static/media/Link.81cb17e7.png"
+  },
+  {
+    "revision": "c9e00cae67cba04c3319e57dedc4a17f",
+    "url": "/project-lsg/static/media/Magda.c9e00cae.png"
+  },
+  {
+    "revision": "aae595a266f09821ef438a01e4fa9393",
+    "url": "/project-lsg/static/media/Master Kohga.aae595a2.png"
+  },
+  {
+    "revision": "0fea86057089144d1c15003802b08990",
+    "url": "/project-lsg/static/media/Mipha.0fea8605.png"
+  },
+  {
+    "revision": "109a49c1f302a636ec322711e684e69a",
+    "url": "/project-lsg/static/media/Oaki.109a49c1.png"
+  },
+  {
+    "revision": "b9bb217b51631902419028d5c82aff03",
+    "url": "/project-lsg/static/media/Patrica.b9bb217b.png"
+  },
+  {
+    "revision": "06f1df8aed15a0183e671f815b948be3",
+    "url": "/project-lsg/static/media/Pelison.06f1df8a.png"
+  },
+  {
+    "revision": "8ebf9bb7b3602efeb27e5fdb6ae97501",
+    "url": "/project-lsg/static/media/Prince Sidon.8ebf9bb7.png"
+  },
+  {
+    "revision": "a9ce698698170cb0bd392d473946a0d4",
+    "url": "/project-lsg/static/media/Revali.a9ce6986.png"
+  },
+  {
+    "revision": "5134f7dcfd140e30e6d0bd8e8bb042d5",
+    "url": "/project-lsg/static/media/Riju.5134f7dc.png"
+  },
+  {
+    "revision": "d7d328483df3e5673f03df9fddc21aeb",
+    "url": "/project-lsg/static/media/Robbie.d7d32848.png"
+  },
+  {
+    "revision": "15e69926b4f5268c6fb4560577c926da",
+    "url": "/project-lsg/static/media/Roscoe.15e69926.png"
+  },
+  {
+    "revision": "16a8a2db34b42f6cc1bfa2d65e7ad7c0",
+    "url": "/project-lsg/static/media/Royal White Horse.16a8a2db.png"
+  },
+  {
+    "revision": "1515720c7af3e2abcd2a1eabb635ec86",
+    "url": "/project-lsg/static/media/Teebo.1515720c.png"
+  },
+  {
+    "revision": "48bc2a26aaf3b9dffed81954fec19f63",
+    "url": "/project-lsg/static/media/Urbosa.48bc2a26.png"
+  }
+]);
